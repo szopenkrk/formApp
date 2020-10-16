899d8b5327d6ad195ed976cad903d110207fbb01
Karol Kochaski <karolkochanski@gmail.com>
2020-10-16 11:50:22 +0200

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import enzyme from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  test('test number of labels', () => {
+    const wrapper = enzyme.shallow(<App />);
+
+    expect(wrapper.find('label')).toHaveLength(5);
+  });
+
+  test('test number of buttons', () => {
+    const wrapper = enzyme.shallow(<App />);
+
+    expect(wrapper.find('.sendButton')).toHaveLength(0);
+  });
+
+  test('test if all inputs exists', () => {
+    const wrapper = enzyme.shallow(<App />);
+    expect(wrapper.find({ name: "firstName" })).toHaveLength(1);
+    expect(wrapper.find({ name: "userName" })).toHaveLength(1);
+    expect(wrapper.find({ name: "password" })).toHaveLength(1);
+    expect(wrapper.find({ name: "repeat" })).toHaveLength(1);
+    expect(wrapper.find({ name: "email" })).toHaveLength(1);
+  })
+
 });
